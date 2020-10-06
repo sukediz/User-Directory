89c2f3d68abfbecdb29be83c34cc37a79ab4fd6b
sukey <sukey.ong1@gmail.com>
2020-10-06 16:48:55 +0800

    Updates

---
+++ precache-manifest.630f2106a37ff50d5d397cb05c95b4a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4640485b9359a8ff46d4724f79cdef",
+    "url": "/User-Directory/index.html"
+  },
+  {
+    "revision": "20bbfddeeed1fe9bb8a9",
+    "url": "/User-Directory/static/css/main.28655e6c.chunk.css"
+  },
+  {
+    "revision": "62de1460bf8d82bf7b27",
+    "url": "/User-Directory/static/js/2.1837b155.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/User-Directory/static/js/2.1837b155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bbfddeeed1fe9bb8a9",
+    "url": "/User-Directory/static/js/main.93cb8028.chunk.js"
+  },
+  {
+    "revision": "b043e11dcffff8b98367",
+    "url": "/User-Directory/static/js/runtime-main.a0744aa0.js"
+  }
+]);
